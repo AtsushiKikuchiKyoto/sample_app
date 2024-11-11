5724e5d835fe23cf5da9e095c9636af265a1ef71
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-11-12 08:10:28 +0900

    try again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-  # if user && user.auth......
+  # if user && user.auth.....
     if user&.authenticate(params[:session][:password])
       reset_session #for security
       log_in user
